54ff8a12fa723a3a5a7744fc5ce60ced6efd3056
Albert <avalls89@gmail.com>
2022-03-11 12:58:12 +0100

    11/03/22

--- project_BuenasPracticas/src/Calculator.php
+++ project_BuenasPracticas/src/Calculator.php
@@ -2,6 +2,12 @@
 
 namespace App;
 
+/**
+ * Retorna la suma de dos numeros enteros
+ * @param int $a
+ * @param int $b
+ * new comment
+ */
 class Calculator
 {
     public function add( int $a, int $b): int
